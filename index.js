f5b097b42edc526969c2d4b2a7d9ddd71539cc7d
LilTzar <eeoerkeedu@gmail.com>
2022-03-03 17:16:43 -0700

    added node modules and did some minor organization and edits

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile("ReadME.md", data) {
+    fs.appendFile()
+}
 
 // TODO: Create a function to initialize app
 function init() {}
