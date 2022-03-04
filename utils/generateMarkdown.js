694c086b8761c684e3321f0daa7a3224b3ddf713
LilTzar <eeoerkeedu@gmail.com>
2022-03-04 09:49:43 -0700

    refined generate funtion for format on readme, working on functions to handle data

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -12,8 +12,39 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+	return `# ${data.title}
+	![Github licence](http://img.shields.io/badge/license-${data.license}-blue.svg)
+	
+	## Description 
+	${data.description}
 
+	## Table of Contents
+	* [Installation](#installation)
+	* [Usage](#usage)
+	* [License](#license)
+	* [Contributing](#contributing)
+	* [Tests](#tests)
+	* [Questions](#questions)
+
+	## Installation 
+	${data.install}
+	
+	## Usage 
+	${data.usage}
+	
+	## License 
+	This project is license under ${data.license}
+	
+	## Contributing 
+	${data.contributors}
+	
+	## Tests
+	${data.test}
+
+	## Questions
+  	Find an issue or have a question? Contact me ${data.email}. 
+	If you liked what you found, you can view more of my projects at: 
+	https://github.com/${data.github}.
 `;
 }
 
